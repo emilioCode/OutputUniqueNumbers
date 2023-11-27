6c8a2150dc5c5aef91a46f52c5391570c8cd6f79
Manuel E Mendez <emilio_mem@hotmail.com>
2023-11-27 14:51:28 -0400

    Add project files.

---
+++ OutputUniqueNumbers/FileContent.cs
@@ -0,0 +1,55 @@
+﻿namespace OutputUniqueNumbers
+{
+    public static class FileContent
+    {
+
+        public static IEnumerable<String> ReadToEnd(string filePath)
+        {
+            List<string> enumerable = new();
+            try
+            {
+                //// Open the file with a StreamReader
+                //using (StreamReader sr = new StreamReader(filePath))
+                //{
+                //    // Read the entire file content
+                //    string content = sr.ReadToEnd();
+
+                //    // Display the content
+                //    return content;
+                //}
+                
+                // Abre el archivo con un StreamReader
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    // Lee cada línea del archivo
+                    while (!sr.EndOfStream)
+                    {
+                        // Lee la línea actual
+                        string line = sr.ReadLine();
+
+                        // Divide la línea en un arreglo usando la coma como delimitador
+                        string[] values = line.Split(',');
+
+                        // Itera a través de los valores e imprímelos
+                        foreach (string value in values)
+                        {
+                            //Console.WriteLine(value);
+                            enumerable.Add(value);
+                        }
+
+                        // Puedes hacer algo con los valores, como almacenarlos en una lista o procesarlos de alguna manera
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                // Handle exceptions, such as file not found
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            return enumerable;
+            
+
+        }
+    }
+}
---
+++ OutputUniqueNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using OutputUniqueNumbers;
+
+Console.Write("Please write the filePath here: ");
+string filePath = "C:\\Users\\Owner\\Desktop\\ISBN_Input_File.txt";// Console.ReadLine();
+
+var content = FileContent.ReadToEnd(filePath);
+
+foreach (var item in content)
+{ 
+    Console.WriteLine();
+    Console.WriteLine($"=================: {item} :=================");
+    var array = new List<char>();
+    for (int i = 0; i < item.Length; i++)
+    {
+        char character = item[i];
+        array.Add(character);
+    }
+
+    Console.Write("its unique numbers are: ");
+    HashSet<char> uniqueNumbers = new HashSet<char>(array);
+    Console.WriteLine(string.Join(", ", uniqueNumbers));
+
+}
+
+
+
